8a738898376b5bdc275c96342724c44c3282e016
Maria Govorukhina <m.govoru@yandex.ru>
2024-08-21 16:04:57 +0500

    feat: add header component

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,1 +1,41 @@
 // header
+'use client';
+import * as React from 'react';
+import Switch from '@mui/material/Switch';
+import Image from 'next/image';
+import './Header.scss';
+import { Button } from '@mui/material';
+export default function Header() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+  return (
+    <div className="header">
+      <div className="header__container">
+        <div>
+          {' '}
+          <Image
+            width={150}
+            height={50}
+            alt="logo"
+            src={'./rs_school_js.svg'}
+          />
+        </div>
+        <div className="header__switch">
+          {' '}
+          {checked ? 'EN' : 'RU'}
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ 'aria-label': 'controlled' }}
+          />
+        </div>
+        <Button variant="contained" href="https://rs.school/">
+          Sign in
+        </Button>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from './components/header/Header';
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
-      {/* <Header/> */}
+      <Header />
       <main className={styles.main}>main page</main>
       {/* <Footer/> */}
     </>
