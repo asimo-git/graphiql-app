5abd4b243a379125bf9c83c45af3c04b7081e213
Rina Asimo <rvolenc@gmail.com>
2024-08-21 10:18:03 +0400

    feat: add footer

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,1 +1,42 @@
-// footer
+import { Link } from '@mui/material';
+import styles from './footer.module.css';
+import Image from 'next/image';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.linkContainer}>
+        Our team:
+        <div>
+          <Link
+            href="https://github.com/mgovoru"
+            underline="hover"
+            className={styles.blockLink}
+          >
+            Maria G.
+          </Link>
+          <Link
+            href="https://github.com/asimo-git"
+            underline="hover"
+            className={styles.blockLink}
+          >
+            {' '}
+            Rina K.
+          </Link>
+          <Link
+            href="https://github.com/drbliman"
+            underline="hover"
+            className={styles.blockLink}
+          >
+            {' '}
+            Vladimir B.
+          </Link>
+        </div>
+      </div>
+      <div>2024</div>
+      <Link href="https://rs.school/courses/reactjs">
+        <Image src="/rs.svg" alt="RS school logo" width={24} height={24} />
+      </Link>
+    </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from './components/footer/Footer';
 import styles from './page.module.css';
 
 export default function Home() {
@@ -5,7 +6,7 @@ export default function Home() {
     <>
       {/* <Header/> */}
       <main className={styles.main}>main page</main>
-      {/* <Footer/> */}
+      <Footer />
     </>
   );
 }
