4790801053184ba8b24342e6737127fab2705687
Maria Govorukhina <m.govoru@yandex.ru>
2024-08-23 18:24:52 +0500

    feat: add styles box

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   };
   return (
     <header className="header">
-      <div className="header__container">
+      <div className="header__container box">
         <div className="header__logo">
           <Link href="./" />{' '}
           <Image width={50} height={50} alt="logo" src={'/logo.jpg'} />
