124b19f2e9ddecd2bf9c2f1ae601b214f92473ff
Maria Govorukhina <m.govoru@yandex.ru>
2024-08-30 12:57:19 +0500

    feat: add changes in form restfull

--- app/components/RESTfull-form/RESTfull-form.tsx
+++ app/components/RESTfull-form/RESTfull-form.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+'use client';
+import React, { ReactElement } from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -7,13 +8,28 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
 import './RESTfull.scss';
+import { JsonView, allExpanded, defaultStyles } from 'react-json-view-lite';
+import 'react-json-view-lite/dist/index.css';
+import { JsonEditor } from 'json-edit-react';
+import { useEffect, useState } from 'react';
 
 const RESTfullForm = () => {
   const [Method, setMethod] = React.useState('');
-
   const handleChange = (event: SelectChangeEvent) => {
     setMethod(event.target.value as string);
   };
+  const [jsonData, setJsonData] = useState({});
+  useEffect(() => setJsonData(jsonData), []);
+  const [elementContent, setElementContent] = useState<ReactElement | null>(
+    null
+  );
+  useEffect(() => {
+    const element = <JsonEditor data={jsonData} setData={setJsonData} />;
+    if (element) {
+      setElementContent(element);
+    }
+  }, [jsonData, setJsonData]);
+
   return (
     <div className="rest__container">
       <h1 className="rest__title">REST Client</h1>
@@ -33,6 +49,8 @@ const RESTfullForm = () => {
               <MenuItem value={30}>PUT</MenuItem>
               <MenuItem value={40}>PATCH</MenuItem>
               <MenuItem value={50}>DELETE</MenuItem>
+              <MenuItem value={50}>HEAD</MenuItem>
+              <MenuItem value={50}>OPTIONS</MenuItem>
             </Select>
           </FormControl>
           <TextField
@@ -69,7 +87,22 @@ const RESTfullForm = () => {
         />
       </div>
       <div className="rest__item">
+        <span>Body:</span>
+        {elementContent}
+      </div>
+      <div className="rest__item item-title">
+        <span>Response</span>{' '}
+      </div>
+      <div className="rest__item">
+        <span>Status:</span>{' '}
+      </div>
+      <div className="rest__item">
         <span>Body:</span>{' '}
+        <JsonView
+          data={jsonData}
+          shouldExpandNode={allExpanded}
+          style={defaultStyles}
+        />
       </div>
     </div>
   );
