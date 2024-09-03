e1b023897730009cc66283b1a10f412d1e0684ef
Maria Govorukhina <m.govoru@yandex.ru>
2024-09-03 12:30:37 +0500

    feat: add function in helpers

--- app/utils/url-restfull.ts
+++ app/utils/url-restfull.ts
@@ -1,8 +1,5 @@
-const stringToBase64 = (str: string) => {
-  const bytes = new TextEncoder().encode(str);
-  const base64 = btoa(String.fromCharCode(...bytes));
-  return base64;
-};
+import { stringToBase64 } from './helpers';
+
 export function urlRESTfull(
   method: string,
   url: string,
@@ -10,11 +7,15 @@ export function urlRESTfull(
   headers: Record<string, string> = {}
 ): string {
   const mainUrl = 'http://localhost:3000';
+
   let needUrl = `${mainUrl}/${method}/${stringToBase64(url)}`;
+
   if (body) {
     needUrl += `/${stringToBase64(JSON.stringify(body))}`;
   }
+
   const queryParams = new URLSearchParams();
+
   for (const [key, value] of Object.entries(headers)) {
     queryParams.append(key, stringToBase64(value));
   }
@@ -22,6 +23,6 @@ export function urlRESTfull(
   if (queryParams.toString()) {
     needUrl += `?${queryParams.toString()}`;
   }
-  //console.log(needUrl);
+
   return needUrl;
 }
