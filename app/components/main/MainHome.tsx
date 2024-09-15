75829486eadcc06ade1850a8542df9c274cbd7ba
Maria Govorukhina <m.govoru@yandex.ru>
2024-09-15 15:11:47 +0500

    refactor: add changes in styles

--- app/components/main/MainHome.tsx
+++ app/components/main/MainHome.tsx
@@ -1,25 +1,30 @@
 'use client';
-import { Link } from '@mui/material';
+import Link from 'next/link';
 import Routers from '../../utils/routes';
 import styles from './Main.module.scss';
 import { auth } from '@/app/services/firebase';
 import Info from '../info/Info';
+import { useTranslation } from 'react-i18next';
 
 export default function MainHome() {
   const name = auth.currentUser?.displayName;
 
+  const { t } = useTranslation();
+
   return (
     <main className={styles.main}>
-      <h1 className={styles.welcome}>{`Welcome, ${name}!`}</h1>
+      <h1 className={styles.welcome}>
+        {t(`Welcome`)}, {`${name}!`}
+      </h1>
       <div className={styles.links}>
-        <Link href={Routers.RESTfull} underline="hover" marginRight={'2rem'}>
-          REST Client
+        <Link href={Routers.RESTfull} className="link">
+          {t('REST Client')}
         </Link>
-        <Link href={Routers.GraphiQL} underline="hover" marginRight={'2rem'}>
-          GraphiQL Client
+        <Link href={Routers.GraphiQL} className="link">
+          {t('GraphiQL Client')}
         </Link>
-        <Link href={Routers.History} underline="hover" marginRight={'2rem'}>
-          History
+        <Link href={Routers.History} className="link">
+          {t('History')}
         </Link>
       </div>
       <Info />
--- app/history/page.tsx
+++ app/history/page.tsx
@@ -2,14 +2,12 @@
 import { useRouter } from 'next/navigation';
 import { useAuthenticated } from '../utils/Auth';
 import Routes from '../utils/routes';
-// import { useTranslation } from 'react-i18next';
 import HistoryList from '../components/History-list/HistoryList';
-import './History.scss';
 
 export default function HistoryPage() {
   const router = useRouter();
+
   const { user, isLoading } = useAuthenticated();
-  // const { t } = useTranslation();
 
   return (
     <>
